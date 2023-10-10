99e45e9b9da918c9c12bd2bac01fe9410de6ab80
thekha3321 <thekha234@gmail.com>
2023-10-10 13:12:33 +0700

    upload file to server

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import likeRouter from './routes/likes.js'
 import commentRouter from './routes/comments.js'
 import postRouter from './routes/posts.js'
 import cors from 'cors'
+import multer from 'multer'
 import cookieParser from "cookie-parser";
 
 const PORT = 8800
@@ -19,6 +20,22 @@ app.use(cors({
   origin: 'http://localhost:3000'
 }))
 app.use(cookieParser())
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '../client/public/upload')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname)
+  }
+})
+
+const upload = multer({ storage: storage })
+
+app.post('/api/upload', upload.single('file'), (req, res) => {
+  const file = req.file;
+  res.status(200).json(file.filename)
+})
 
 app.use('/api/users', userRouter)
 app.use('/api/auth/', authRouter)
--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -2,6 +2,8 @@ import Post from "../post/Post";
 import "./posts.scss";
 import { makeRequest } from "../../axios";
 import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const Posts = () => {
   const [data, setData] = useState([]);
@@ -11,15 +13,25 @@ const Posts = () => {
     await makeRequest
     .get("/posts")
     .then((response) => {
-      setData(response.data);
+      setData(response.data.reverse());
     })
     .catch((error) => {
-      setError("Lỗi:", error);
+      setError(error);
     });
   }
   useEffect(()=> {
     getPosts();
+    
   }, [])
+  // const { isLoading, error, data } = useQuery({
+  //   queryKey: ['posts'],
+  //   queryFn: () =>
+  //     axios.get('http://localhost:8800/api/posts').then(
+  //       (res) => res.json(),
+  //     ),
+  // })
+  // console.log(`data :${data}`);
+  // console.log(`error :${error}`);
   return (
     <div className="posts">
       {data.map((post) => <Post post={post} key={post.id} />)}
